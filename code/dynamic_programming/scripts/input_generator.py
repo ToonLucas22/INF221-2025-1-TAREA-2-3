5f099ad6bd9d1baec0d5e603ccaeb4e88c0cbd58
ToonLucas22 <lucas.ignacio.morrison.osorio@gmail.com>
2025-06-17 13:41:51 -0400

    Update input_generator.py

---
+++ code/dynamic_programming/scripts/input_generator.py
@@ -0,0 +1,50 @@
+from random import randint
+
+def getRandCharFromSet(set):
+    randPos = randint(0, len(set)-1)
+    randChar = set[randPos]
+    return randChar
+
+D = [100, 10000, 100000, 1000000]
+E = [20, 50, 80]
+charsets = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"]
+nCasesPerCombo = 150
+
+outputFormat = "{}_{}_{}.dat"
+lineFormat = "{} {}\n"
+for d in D:
+    for e in E:
+        setPos = 0
+        while setPos < len(charsets):
+            try:
+                output = open(outputFormat.format(d, e, setPos), "x")
+            except FileExistsError:
+                output = open(outputFormat.format(d, e, setPos), "w")
+            finally:
+                output.write(str(nCasesPerCombo)+"\n")
+            
+            i = 0
+            while i < nCasesPerCombo:
+                s1 = ""
+                while (len(s1) < d):
+                    charTo = getRandCharFromSet(charsets[setPos])
+                    s1 += charTo
+                s2 = str(s1)
+                j = 0
+                while (j < len(s2)):
+                    roll = randint(1, 100)
+                    if (roll <= e):
+                        stalin = randint(1, 10)
+                        if stalin == 10:
+                            s2 = s2[:j]+s2[j+1:]
+                        else:
+                            charTo = getRandCharFromSet(charsets[setPos])
+                            while charTo[0] == s2[j]:
+                                charTo = getRandCharFromSet(charsets[setPos])
+                            s2 =s2[:j]+charTo+s2[j+1:]
+                            j += 1
+                output.write(lineFormat.format(len(s1), s1))
+                output.write(lineFormat.format(len(s2), s2))
+                i += 1
+            output.close()
+            setPos += 1
